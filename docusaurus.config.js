04ba54a20c29b5ce7cc46f176deaac1d00fb2633
kblaha <kblaha@gmail.com>
2023-11-27 10:33:02 -0800

    Point edit url to our repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,15 +44,13 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/kblaha/docusaurus.demo/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/kblaha/docusaurus.demo/',
         },
         theme: {
           customCss: './src/css/custom.css',
