1b517659b2c310603187fecd6e9a97f40f4100e7
kblaha <kblaha@gmail.com>
2023-11-27 09:49:29 -0800

    Update config

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This website is built using [Docusaurus 2](https://docusaurus.io/), a modern sta
 ### Installation
 
 ```
-$ yarn
+$ yarn 
 ```
 
 ### Local Development
@@ -14,12 +14,30 @@ $ yarn
 $ yarn start
 ```
 
+or 
+
+```
+$ npx docusaurus start
+```
+
+or 
+
+```
+$ npm run start
+```
+
 This command starts a local development server and opens up a browser window. Most changes are reflected live without having to restart the server.
 
 ### Build
 
 ```
-$ yarn build
+$ yarn build 
+```
+
+or
+
+```
+npm run build
 ```
 
 This command generates static content into the `build` directory and can be served using any static contents hosting service.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Demo Site',
+  tagline: 'Customizable tagline here',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kblaha', // Usually your GitHub org/user name.
+  projectName: 'docusaurus.demo', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -66,9 +66,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Demo Site',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Demo Site Logo',
           src: 'img/logo.svg',
         },
         items: [
@@ -80,7 +80,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/kblaha/docusaurus.demo',
             label: 'GitHub',
             position: 'right',
           },
@@ -124,7 +124,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/kblaha/docusaurus.demo',
               },
             ],
           },
